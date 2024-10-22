19ec9d7307f14b9f59a720973c1bc67e1cdb805b
reimo <reimo.nurges@tktk.ee>
2024-10-22 09:13:05 +0300

    First comment

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿int math, biology, chem;
+
+Console.WriteLine("Sisesta mata tulemus: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisesta bioloogia tulemus: ");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisestage keemia tulemus: ");
+chem = Int32.Parse(Console.ReadLine());
+
+if  (math >= 90 && biology >= 90 && chem >= 90)
+{
+    Console.WriteLine("Concrats");
+}
+else
+{
+    Console.WriteLine("oops");
+
+}
---
+++ ANDRanges/Program.cs
@@ -0,0 +1,38 @@
+﻿// temp <= 0 - freezing cold
+// temp > 0 and <= 10 - cold
+// temp > 10 and <= 15 - chilly
+// temp > 15 and <= 20 - warm
+
+Console.WriteLine("Temp");
+int temp = Int32.Parse(Console.ReadLine());
+
+
+if  (temp >= 30)
+{
+    Console.WriteLine("boiling hot");
+}
+else if (temp >= 20 &&  temp < 30)
+{
+    Console.WriteLine("hot");
+
+}
+else if (temp >= 15 && temp < 20)
+{
+    Console.WriteLine("warm");
+
+}
+else if (temp >= 10 && temp < 15)
+{
+    Console.WriteLine("Chilly");
+
+}
+else if (temp > 0 && temp < 10)
+{
+    Console.WriteLine("cold");
+
+}
+else 
+{
+    Console.WriteLine("Freezing cold");
+
+}
